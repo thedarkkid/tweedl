33e3acb5649762b5abcff96fd25c10ddb5e5c1b7
thedarkkid <thedarkkid.codes@gmail.com>
2020-04-16 23:45:09 +0400

    editing sidebar and header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Header/>
+    <Sidebar/>
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/layouts/Header";
+import Sidebar from "@/components/layouts/Sidebar";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Sidebar,
+    Header,
   }
 }
 </script>
